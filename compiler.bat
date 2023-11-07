783af95defbccf8f6e3003e27ebf520f70c9952d
Atutum <92112526+bablo9990@users.noreply.github.com>
2023-11-07 14:05:04 +0300

    Add files via upload

---
+++ compiler.bat
@@ -0,0 +1,3 @@
+cd D:\Рабочий стол\Admin\Important {ADMIN}\Programming Projects\Python\Windows Super Activator
+D:
+pyuic5 -x main.ui -o compiler.py
---
+++ run.bat
@@ -0,0 +1,4 @@
+cd D:\Рабочий стол\Admin\Important {ADMIN}\Programming Projects\Python\Windows Super Activator
+D:
+python compiler.py
+pause
